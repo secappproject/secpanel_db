f123eca6bb9d4e2ec1700f0e0d9351d85f4426b2
Arshad <arshad@MacBook-Pro-Arshad.local>
2025-10-07 16:06:22 +0700

    export issue additional sr

--- main.go
+++ main.go
@@ -115,6 +115,38 @@ type ChatMessage struct {
 	RepliedIssueID *int       `json:"replied_issue_id,omitempty"` // Pointer dan omitempty
 	CreatedAt     time.Time `json:"created_at"`
 }
+
+type IssueForExport struct {
+	PanelNoPp   string
+	PanelNoWbs  sql.NullString
+	PanelNoPanel sql.NullString
+	IssueID     int
+	Title       string
+	Description string
+	Status      string
+	CreatedBy   string
+	CreatedAt   time.Time
+}
+
+type CommentForExport struct {
+	IssueID        int
+	Text           string
+	SenderID       string
+	ReplyToCommentID sql.NullString
+}
+
+type AdditionalSRForExport struct {
+	PanelNoPp   string
+	PanelNoWbs  sql.NullString
+	PanelNoPanel sql.NullString
+	PoNumber    string
+	Item        string
+	Quantity    int
+	Supplier    sql.NullString
+	Status      string
+	Remarks     string
+}
+
 type Issue struct {
 	ID          int       `json:"issue_id"`
 	ChatID      int       `json:"chat_id"`
@@ -2283,6 +2315,73 @@ func (a *App) getFilteredDataForExport(r *http.Request) (map[string]interface{},
 		result["components"], _ = fetchInAs(tx, "components", "panel_no_pp", relevantPanelIds, func() interface{} { return &Component{} })
 		result["palet"], _ = fetchInAs(tx, "palet", "panel_no_pp", relevantPanelIds, func() interface{} { return &Palet{} })
 		result["corepart"], _ = fetchInAs(tx, "corepart", "panel_no_pp", relevantPanelIds, func() interface{} { return &Corepart{} })
+		// Query untuk Issues
+		issueQuery := `
+			SELECT p.no_pp, p.no_wbs, p.no_panel, i.id, i.title, i.description, i.status, i.created_by, i.created_at 
+			FROM issues i
+			JOIN chats ch ON i.chat_id = ch.id
+			JOIN panels p ON ch.panel_no_pp = p.no_pp
+			WHERE p.no_pp = ANY($1)
+			ORDER BY p.no_pp, i.created_at`
+		issueRows, err := tx.Query(issueQuery, pq.Array(relevantPanelIds))
+		if err != nil {
+			log.Printf("Gagal query issues for export: %v", err)
+		} else {
+			var issues []IssueForExport
+			for issueRows.Next() {
+				var issue IssueForExport
+				if err := issueRows.Scan(&issue.PanelNoPp, &issue.PanelNoWbs, &issue.PanelNoPanel, &issue.IssueID, &issue.Title, &issue.Description, &issue.Status, &issue.CreatedBy, &issue.CreatedAt); err == nil {
+					issues = append(issues, issue)
+				}
+			}
+			result["issues"] = issues
+			issueRows.Close()
+		}
+
+		// Query untuk Comments
+		commentQuery := `
+			SELECT ic.issue_id, ic.text, ic.sender_id, ic.reply_to_comment_id
+			FROM issue_comments ic
+			JOIN issues i ON ic.issue_id = i.id
+			JOIN chats ch ON i.chat_id = ch.id
+			WHERE ch.panel_no_pp = ANY($1)
+			ORDER BY ic.issue_id, ic.timestamp`
+		commentRows, err := tx.Query(commentQuery, pq.Array(relevantPanelIds))
+		if err != nil {
+			log.Printf("Gagal query comments for export: %v", err)
+		} else {
+			var comments []CommentForExport
+			for commentRows.Next() {
+				var comment CommentForExport
+				if err := commentRows.Scan(&comment.IssueID, &comment.Text, &comment.SenderID, &comment.ReplyToCommentID); err == nil {
+					comments = append(comments, comment)
+				}
+			}
+			result["comments"] = comments
+			commentRows.Close()
+		}
+		
+		// Query untuk Additional SRs
+		srQuery := `
+			SELECT p.no_pp, p.no_wbs, p.no_panel, asr.po_number, asr.item, asr.quantity, asr.supplier, asr.status, asr.remarks
+			FROM additional_sr asr
+			JOIN panels p ON asr.panel_no_pp = p.no_pp
+			WHERE p.no_pp = ANY($1)
+			ORDER BY p.no_pp, asr.created_at`
+		srRows, err := tx.Query(srQuery, pq.Array(relevantPanelIds))
+		if err != nil {
+			log.Printf("Gagal query srs for export: %v", err)
+		} else {
+			var srs []AdditionalSRForExport
+			for srRows.Next() {
+				var sr AdditionalSRForExport
+				if err := srRows.Scan(&sr.PanelNoPp, &sr.PanelNoWbs, &sr.PanelNoPanel, &sr.PoNumber, &sr.Item, &sr.Quantity, &sr.Supplier, &sr.Status, &sr.Remarks); err == nil {
+					srs = append(srs, sr)
+				}
+			}
+			result["additional_srs"] = srs
+			srRows.Close()
+		}
 	} else {
 		result["busbars"], result["components"], result["palet"], result["corepart"] = []Busbar{}, []Component{}, []Palet{}, []Corepart{}
 	}
